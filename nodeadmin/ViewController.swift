5f9d04558ecd743791e81219d29f88d042cb8344
Taylor Lehman <taylorleh@mac.com>
2016-01-26 22:35:58 -0800

    Initial Commit

---
+++ nodeadmin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nodeadmin
+//
+//  Created by Taylor Lehman on 1/26/16.
+//  Copyright © 2016 Taylor Lehman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
